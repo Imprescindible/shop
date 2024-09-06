53a713e42134fbc2445f6fe1432a79694a179d7c
Imprescindible <swc.ignore@gmail.com>
2024-09-06 20:27:06 +0300

    add routes

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/login';
+import Item from './pages/item/item';
+import Favourite from './pages/favourite/favourite';
+import Home from './pages/home/home';
+import Header from './components/header';
+
 function App() {
-  return <div className="App">Title :Hi</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/favorite" element={<Favourite />} />
+          <Route exact path="/shop-item/:id" element={<Item />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ shop/src/pages/favourite/favourite.js
@@ -0,0 +1,5 @@
+const Favourite = () => {
+  return <>asd</>;
+};
+
+export default Favourite;
